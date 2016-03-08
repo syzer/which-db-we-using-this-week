06e5f9f5a0c6cff575e2ed7c46db5434bfd06552
syzer3@gmail.com <syzer3@gmail.com>
2016-03-08 09:43:14 +0100

    added image

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const _ = require('lodash');
 const dbs = [
     {name: 'RethinkDb', hipsterFeatures: ['Streams']},
@@ -11,5 +13,10 @@ const dbs = [
     {name: 'ElasticSearch', hipsterFeatures: ['Data lake']}
 ];
 const thisWeekDb = _.sample(dbs);
-const because = _.sample(thisWeekDb.hipsterFeatures).toLowerCase();
-console.log(`This week we use ${thisWeekDb.name}, because having a ${thisWeekDb.hipsterFeatures} is crucial to our business.`);
+const noReason = _.flow(_.sample, _.toLower)(thisWeekDb.hipsterFeatures);
+console.log(`This week we use ${thisWeekDb.name}, \
+because having a ${noReason} is crucial to our business.`);
+
+const img2ascii = require('img2ascii')({
+    img: './assets/snafu.gif', cols: 90
+}).pipe(process.stdout);
